e5cd74c3504be889de570b210951ad606b3ac4f6
SreeshmaMohanan <ppsreeshma10@mail.com>
2023-11-15 21:17:23 +0530

    server

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,6 @@
+const { sign } = require('jsonwebtoken');
 const users=require('../Models/userSchema')
+const jwt=require('jsonwebtoken')
 //register
 exports.register =async(req,res)=>{
     console.log('inside register controller function');
@@ -22,10 +24,20 @@ exports.register =async(req,res)=>{
 }
 //login
 exports.login = async (req, res) => {
-    let user;
-    const { email, password } = req.body;
-    try {
-        user = await users.findOne({ email });
-        
+   console.log("inside login function");
+   const {email,password}=req.body
+   try {
+    const existingUser = await users.findOne({email,password })
+    if(existingUser){
+        const token= jwt.sign({userId:existingUser._id},"supersecretkey12345") 
+        res.status(200).json({
+           existingUser,token
+        })
+    }else{
+        res.status(404).json('Invalid Login Details')
     }
+
+   }catch(err){
+    res.status(401).json(`login api failed error:${err}`)
+   }
 }
--- Routes/router.js
+++ Routes/router.js
@@ -3,7 +3,7 @@ const router =new express.Router()
 const userController =require('../Controllers/userController')
 // create path register API
 router.post('/user/register',userController.register)
-//path tologin api
-router.post("/user/login",userController.login);
+//path to login api
+router.post('/user/login',userController.login);
 
 module.exports =router
